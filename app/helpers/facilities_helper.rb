15b99aaeaa05818db54a555c812dd99ddc206eb4
whitecaps <white.caps@yahoo.com>
2015-01-10 14:43:43 -0800

    styled buttons and added remaining glyphicons
    need to add distances. pass from model as tuple maybe.

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -1,2 +1,28 @@
 module FacilitiesHelper
+
+	def display_services(services_str)
+		content_arr = ""
+		services_arr = services_str.split(" ")
+		services_arr.each do |s|
+			case s
+			when "Shelter"
+   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-home shelter-colour-index")
+   			when "Food"
+   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-cutlery food-colour-index")
+   			when "Medical"
+   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-heart-empty medical-colour-index")
+   			when "Hygiene"
+   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-tint hygiene-colour-index")
+   			else
+   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-globe technology-colour-index")
+
+			end #ends case
+		end
+
+		return content_arr.html_safe
+	end
+
+	#def display_distance(fac)
+
+	#end
 end
