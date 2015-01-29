d5aebee428719960aabef4d1fce89d70c3593e8e
whitecaps <white.caps@yahoo.com>
2015-01-28 20:17:29 -0800

    added precompile to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Linkvan::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
