26e9e69019005bea849739e45d04b38fe288847d
whitecaps <white.caps@yahoo.com>
2015-08-12 11:05:22 -0700

    tweaked margins to 1 in direction css

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -12,7 +12,7 @@ class Facility < ActiveRecord::Base
 
 		ulat = ulat.to_d
 		ulong = ulong.to_d
-		
+
 		#first query db for any facility whose services contains the service_query
 		#and store in array arr
 		facility = Facility.select("services", "id")
@@ -40,14 +40,14 @@ class Facility < ActiveRecord::Base
 			#the problem: after Aachen Token goes through, UBCLE2 does not enter the arr.each loop even though Alabama does.
 			#for some reason i'm getting a null pointer when i hit UBCLE2 in the while loop. Alabama executes find though.
 
-		
+
 			temp_arr = Array.new
 			temp_arr = arr
 
 			$i = 0
 
 			while $i < temp_arr.count  do
-				
+
 			case day
 				when 0
 					if (8.hours.ago.hour.to_i < arr[$i].startssun_at.hour.to_i) || (8.hours.ago.hour.to_i > arr[$i].endssun_at.hour.to_i)
@@ -169,7 +169,7 @@ class Facility < ActiveRecord::Base
 			$i = 0
 
 			while $i < temp_arr.count  do
-				
+
 			case day
 				when 0
 					if (8.hours.ago.hour.to_i > arr[$i].startssun_at.hour.to_i) && (8.hours.ago.hour.to_i < arr[$i].endssun_at.hour.to_i)
@@ -216,7 +216,7 @@ class Facility < ActiveRecord::Base
 				when 3
 
 					if (8.hours.ago.hour.to_i > arr[$i].startswed_at.hour.to_i) && (8.hours.ago.hour.to_i < arr[$i].endswed_at.hour.to_i)
-						
+
 						t = arr[$i]
 						temp_arr.delete(t)
 						$i-=1
@@ -279,7 +279,7 @@ class Facility < ActiveRecord::Base
 			end #ends while loop
 		elsif ((prox=="Name") && (open=="Yes"))
 			arr = arr.sort_by {|f| f[:name]}
-			
+
 			temp_arr = Array.new
 			temp_arr = arr
 
@@ -287,7 +287,7 @@ class Facility < ActiveRecord::Base
 
 
 			while $i < temp_arr.count  do
-				
+
 			case day
 				when 0
 					if (8.hours.ago.hour.to_i < arr[$i].startssun_at.hour.to_i) || (8.hours.ago.hour.to_i > arr[$i].endssun_at.hour.to_i)
@@ -398,14 +398,14 @@ class Facility < ActiveRecord::Base
 
 		elsif ((prox=="Name") && (open=="No"))
 			arr = arr.sort_by {|f| f[:name]}
-			
+
 			temp_arr = Array.new
 			temp_arr = arr
 
 			$i = 0
 
 			while $i < temp_arr.count  do
-				
+
 			case day
 				when 0
 					if (8.hours.ago.hour.to_i > arr[$i].startssun_at.hour.to_i) && (8.hours.ago.hour.to_i < arr[$i].endssun_at.hour.to_i)
@@ -535,38 +535,38 @@ end
 def self.haversine(lat1, long1, lat2, long2)
   dtor = Math::PI/180
   r = 6378.14*1000 #delete 1000 to get kms
- 
-  rlat1 = lat1 * dtor 
-  rlong1 = long1 * dtor 
-  rlat2 = lat2 * dtor 
-  rlong2 = long2 * dtor 
- 
+
+  rlat1 = lat1 * dtor
+  rlong1 = long1 * dtor
+  rlat2 = lat2 * dtor
+  rlong2 = long2 * dtor
+
   dlon = rlong1 - rlong2
   dlat = rlat1 - rlat2
- 
+
   a = power(Math::sin(dlat/2), 2) + Math::cos(rlat1) * Math::cos(rlat2) * power(Math::sin(dlon/2), 2)
   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
   d = r * c
- 
+
   return d
 end
 
 def self.haversine_km(lat1, long1, lat2, long2)
   dtor = Math::PI/180
   r = 6378.14
- 
-  rlat1 = lat1 * dtor 
-  rlong1 = long1 * dtor 
-  rlat2 = lat2 * dtor 
-  rlong2 = long2 * dtor 
- 
+
+  rlat1 = lat1 * dtor
+  rlong1 = long1 * dtor
+  rlat2 = lat2 * dtor
+  rlong2 = long2 * dtor
+
   dlon = rlong1 - rlong2
   dlat = rlat1 - rlat2
- 
+
   a = power(Math::sin(dlat/2), 2) + Math::cos(rlat1) * Math::cos(rlat2) * power(Math::sin(dlon/2), 2)
   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
   d = r * c
- 
+
   return d
 end
 
@@ -585,7 +585,7 @@ def self.bubble_sort(list, alist )
         alist[i], alist[i+1] = alist[i+1], alist[i]
         swapped = true
       end
-    end    
+    end
   end
 
   alist
@@ -602,4 +602,3 @@ end
 
 
 end #ends class
-
