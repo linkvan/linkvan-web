126851c833c4796d8163ec8fedcd3bf93ae72054
whitecaps <white.caps@yahoo.com>
2015-11-14 20:08:47 -0800

    replaced icons in services

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -6,17 +6,17 @@ module FacilitiesHelper
 		services_arr.each do |s|
 			case s
 			when "Shelter"
-   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-home shelter-colour-index")
+   				content_arr += content_tag(:i, nil, class: "icon-sleepshelter_linkvan shelter-colour-index")
    			when "Food"
    				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-cutlery food-colour-index")
    			when "Medical"
-   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-heart-empty medical-colour-index")
+   				content_arr += content_tag(:i, nil, class: "icon-Medical_linkvan medical-colour-index")
    			when "Hygiene"
-   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-tint hygiene-colour-index")
+   				content_arr += content_tag(:i, nil, class: "icon-hygiene_linkvan hygiene-colour-index")
    			when "Technology"
-   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-globe technology-colour-index")
+   				content_arr += content_tag(:i, nil, class: "icon-display technology-colour-index")
    			else
-   				content_arr += content_tag(:i, nil, class: "glyphicon glyphicon-briefcase medical-colour-index")
+   				content_arr += content_tag(:i, nil, class: "icon-Advocacy_linkvan medical-colour-index")
 
 			end #ends case
 		end
