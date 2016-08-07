01898b54976ca2e0a94e2257d3ece6b7d29247dd
Felix Tso <Felix@Felixs-MacBook-Pro.local>
2016-08-07 13:50:42 -0700

    testing UserMailer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,9 @@ class MessagesController < ApplicationController
     #@user = User.find(session[:user_id])
 
     if @message.valid?
-      redirect_to "https://intersteller500.herokuapp.com"
+      current_user.activation_email_sent = true
+      current_user.save
+      redirect_to current_user, notice: "Your messages has been sent."
     else
       flash[:alert] = "An error occurred while delivering this message."
       render :new
