fcece4667a2dbf699b215632790ec929ce862e8e
Felix Tso <Felix@Felixs-MacBook-Pro.local>
2016-08-07 12:55:09 -0700

    checking if user is saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,6 @@ class UsersController < ApplicationController
 	def create
   		@user = User.new(user_params)
   		if @user.save
-        session[:user_id] = @user.id
-    		redirect_to @user, notice: "Thanks for signing up!"
-        UserMailer.welcome_email(@user).deliver
   		else
         flash.now[:error] = @user.errors.full_messages
     		render :new
