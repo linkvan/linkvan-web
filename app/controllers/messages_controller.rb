8260f89e5f7b2062f67ba88a3499b3bf0aa10403
Felix Tso <Felix@Felixs-MacBook-Pro.local>
2016-08-07 14:26:03 -0700

    Updated email message to our linkvan team. Testing email

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,7 @@ class MessagesController < ApplicationController
     #@user = User.find(session[:user_id])
 
     if @message.valid?
+      UserMailer.new_message(@message, current_user).deliver
       current_user.activation_email_sent = true
       current_user.save
       redirect_to current_user, notice: "Your messages has been sent."
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "linkvanca@gmail.com"
- 
+
   def welcome_email(user)
   	@url  = 'http://linkvan.ca'
     @user = user
-    mail(to: @user.email, 
+    mail(to: @user.email,
     	subject: 'Welcome to LinkVan!')
   end
 
@@ -12,8 +12,8 @@ class UserMailer < ActionMailer::Base
     @message = message
     @user = user
 
-    
-    mail(to: "info@linkvan.ca",
+
+    mail(to: "f.tso11200@gmail.com",
       subject: "Activation Request from #{@user.name}")
 
   end
