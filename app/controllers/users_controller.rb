32bffb0bf4cfd04f7b807d1cccb4712bcf35d99e
Felix Tso <Felix@Felixs-MacBook-Pro.local>
2016-08-07 13:08:40 -0700

    testing redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,15 +17,8 @@ class UsersController < ApplicationController
 	end
 
 	def create
-  		@user = User.new(user_params)
-  		if @user.save
-        session[:user_id] = @user.id
+  		
     		redirect_to @user, notice: "Thanks for signing up!"
-        UserMailer.welcome_email(@user).deliver
-  		else
-        flash.now[:error] = @user.errors.full_messages
-    		render :new
-  		end
 	end
 
 	def edit
