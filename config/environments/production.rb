6ac3f10aa6dec3ce284944b2963bc8b307c862c7
Felix Tso <Felix@Felixs-MacBook-Pro.local>
2016-08-09 18:10:48 -0700

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Linkvan::Application.configure do
 
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { :host => 'www.interstellar500.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'https://interstellar500.herokuapp.com/' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
