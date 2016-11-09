ffc113fe42fcaa8551f5f4643e01fd9efb77cc9a
Felix Tso <Felix@Felixs-MacBook-Pro.local>
2016-11-09 15:14:28 -0800

    migration to add default value

---
+++ db/migrate/20161109231325_add_default_value_to_admin.rb
@@ -0,0 +1,9 @@
+class AddDefaultValueToAdmin < ActiveRecord::Migration
+  def up
+  change_column :users, :admin, :boolean, :default => false
+end
+
+def down
+  change_column :users, :admin, :boolean, :default => nil
+end
+end 
