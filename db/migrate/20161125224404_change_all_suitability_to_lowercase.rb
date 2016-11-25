8dc9641185059d74129c74c6d9f90b7b3f89e195
Felix Tso <Felix@dhcp-128-189-219-214.ubcsecure.wireless.ubc.ca>
2016-11-25 15:04:27 -0800

    changed migration to change all permutations of suitability

--- db/migrate/20161125224404_change_all_suitability_to_lowercase.rb
+++ db/migrate/20161125224404_change_all_suitability_to_lowercase.rb
@@ -5,10 +5,47 @@ class ChangeAllSuitabilityToLowercase < ActiveRecord::Migration
       case f.suitability
       when "Children Youth"
         f.suitability = "children youth"
+        f.save
       when "Children Youth Adults"
         f.suitability = "children youth adults"
+        f.save
       when "Children Youth Adults Seniors"
         f.suitability = "children youth adults seniors"
+        f.save
+      when "Children Adults"
+        f.suitability = "children adults"
+        f.save
+      when "Children Seniors"
+        f.suitability = "children seniors"
+        f.save
+      when "Children Adults Seniors"
+        f.suitability = "children adults seniors"
+        f.save
+      when "Children Youth Seniors"
+        f.suitability = "children youth seniors"
+        f.save
+      when "Youth"
+        f.suitability = "youth"
+        f.save
+      when "Youth Adults"
+        f.suitability = "youth adults"
+        f.save
+      when "Youth Seniors"
+        f.suitability = "youth seniors"
+        f.save
+      when "Youth Adults Seniors"
+        f.suitability = "youth adults seniors"
+        f.save
+      when "Adults"
+        f.suitability = "adults"
+        f.save
+      when "Adults Seniors"
+        f.suitability = "adults seniors"
+        f.save
+      when "Seniors"
+        f.suitability = "seniors"
+        f.save
+      when
       else
       end
     end
