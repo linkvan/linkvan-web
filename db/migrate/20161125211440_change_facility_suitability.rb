63634e658b8d29d4dfe4f785779271f6b02dbebf
Felix Tso <Felix@dhcp-128-189-219-214.ubcsecure.wireless.ubc.ca>
2016-11-25 14:30:04 -0800

    changed migration file

--- db/migrate/20161125211440_change_facility_suitability.rb
+++ db/migrate/20161125211440_change_facility_suitability.rb
@@ -1,16 +1,20 @@
 class ChangeFacilitySuitability < ActiveRecord::Migration
   def up
+    change_column :facilities, :suitability, :string
     Facility do |f|
       if f.suitability == "Children"
         f.suitability = "children"
+        f.save
       end
     end
   end
 
   def down
+    change_column :facilities, :suitability, :string
     Facility do |f|
       if f.suitability == "children"
         f.suitability = "Children"
+        f.save
       end
     end
   end
