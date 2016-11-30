29827a584a94ad45a0fb60e0fee15c568cce5b5a
Andre Souza <andresouza4@gmail.com>
2016-11-29 21:57:57 -0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20161109231325) do
 
   create_table "anaylitics", force: :cascade do |t|
