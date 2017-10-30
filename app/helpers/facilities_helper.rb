694c6ac4e3f84fd37c427621faacad92562a8882
Andre Souza <andresouza4@gmail.com>
2017-10-29 19:05:34 -0700

    Adjusted icons and labels size

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -104,21 +104,21 @@ module FacilitiesHelper
 		services_arr.each do |s|
 			case s
 			when "Shelter"
-					content_arr += content_tag(:i, inline_svg('icons/shelter.svg', size: '25px'), class: "linkvan-icon service", id: "Shelter", onclick: "moreInfo('shelter');")
+					content_arr += content_tag(:i, inline_svg('icons/shelter.svg', size: '30px'), class: "linkvan-icon service", id: "Shelter", onclick: "moreInfo('shelter');")
    			when "Food"
-   				content_arr += content_tag(:i, inline_svg('icons/cutlery.svg', size: '25px'), class: "linkvan-icon service", id: "Food", onclick: "moreInfo('food');")
+   				content_arr += content_tag(:i, inline_svg('icons/cutlery.svg', size: '30px'), class: "linkvan-icon service", id: "Food", onclick: "moreInfo('food');")
    			when "Medical"
-   				content_arr += content_tag(:i, inline_svg('icons/medical.svg', size: '25px'), class: "linkvan-icon service", id: "Medical", onclick: "moreInfo('medical');")
+   				content_arr += content_tag(:i, inline_svg('icons/medical.svg', size: '30px'), class: "linkvan-icon service", id: "Medical", onclick: "moreInfo('medical');")
    			when "Hygiene"
-   				content_arr += content_tag(:i, inline_svg('icons/hygiene.svg', size: '25px'), class: "linkvan-icon service", id: "Hygiene", onclick: "moreInfo('hygiene');")
+   				content_arr += content_tag(:i, inline_svg('icons/hygiene.svg', size: '30px'), class: "linkvan-icon service", id: "Hygiene", onclick: "moreInfo('hygiene');")
    			when "Technology"
-					content_arr += content_tag(:i, inline_svg('icons/technology.svg', size: '25px'), class: "linkvan-icon service", id: "Technology", onclick: "moreInfo('technology');")
+					content_arr += content_tag(:i, inline_svg('icons/technology.svg', size: '30px'), class: "linkvan-icon service", id: "Technology", onclick: "moreInfo('technology');")
 				when "Legal"
-					content_arr += content_tag(:i, inline_svg('icons/advocacy.svg', size: '25px'), class: "linkvan-icon service", id: "Legal", onclick: "moreInfo('legal');")
+					content_arr += content_tag(:i, inline_svg('icons/advocacy.svg', size: '30px'), class: "linkvan-icon service", id: "Legal", onclick: "moreInfo('legal');")
 				when "Learning"
-					content_arr += content_tag(:i, inline_svg('icons/learning.svg', size: '25px'), class: "linkvan-icon service", id: "Learning", onclick: "moreInfo('learning');")
+					content_arr += content_tag(:i, inline_svg('icons/learning.svg', size: '30px'), class: "linkvan-icon service", id: "Learning", onclick: "moreInfo('learning');")
 				when "Training_Services"
-					content_arr += content_tag(:i, inline_svg('icons/crisis.svg', size: '25px'), class: "linkvan-icon service", id: "Training",  onclick: "moreInfo('training');")
+					content_arr += content_tag(:i, inline_svg('icons/crisis.svg', size: '30px'), class: "linkvan-icon service", id: "Training",  onclick: "moreInfo('training');")
 				#else add error case?
 			end #ends case
 		end
