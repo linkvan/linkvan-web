8cc93120975fd21584fa96c815819f43599221ba
Felix Tso <f.tso11200@gmail.com>
2020-01-07 00:31:27 -0800

    Fix for running rake db migrate. PG error occurs when converting string to decimal without typecasting.

--- db/migrate/20170311175641_change_analytic_facility_type.rb
+++ db/migrate/20170311175641_change_analytic_facility_type.rb
@@ -1,11 +1,9 @@
 class ChangeAnalyticFacilityType < ActiveRecord::Migration
   def up
     change_column :analytics, :facility, 'decimal USING CAST(facility AS decimal)'
-    # change_column :analytics, :facility, :decimal
   end
 
   def down
     change_column :analytics, :facility, :string
-    # change_column :analytics, :facility, :string
   end
 end
