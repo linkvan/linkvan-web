5646cb14ac2000ce0080c9c8b8cf2cd2d1046ca3
Fabio Lima <fabionl@gmail.com>
2020-02-23 12:46:52 -0800

    Changed deprecated render :nothing to head on controllers.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -20,7 +20,7 @@ private
 		unless current_user
 			# session[:intended_url] = request.url
 			# redirect_to new_session_url, alert: "Please sign in first"
-			render nothing: true, status: :unauthorized
+			head :unauthorized
 		end
 	end
 
@@ -32,7 +32,7 @@ private
 
 	def require_admin
 		unless current_user_admin?
-			render nothing: true, status: :unauthorized
+			head :unauthorized
     		# redirect_to root_url, alert: "Unauthorized access!"
 		end
 	end
