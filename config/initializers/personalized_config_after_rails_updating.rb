6491ffb0ad317a1dd0d0009323380e417e7f2168
Fabio Lima <fabionl@gmail.com>
2020-02-23 11:40:56 -0800

    Changes to app config following new_framework_defaults' lead

--- config/initializers/personalized_config_after_rails_updating.rb
+++ config/initializers/personalized_config_after_rails_updating.rb
@@ -16,4 +16,25 @@ Rails.application.configure do
     # config.assets.enabled = true
 
     config.active_record.time_zone_aware_types = [:datetime, :time]
+
+    ###########################
+    # Changes made to new_framework_defaults.rb from Rails 4.2 to Rails 5.0
+    # Enable per-form CSRF tokens. Previous versions had false.
+    Rails.application.config.action_controller.per_form_csrf_tokens = true
+
+    # Enable origin-checking CSRF mitigation. Previous versions had false.
+    Rails.application.config.action_controller.forgery_protection_origin_check = true
+
+    # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
+    # Previous versions had false.
+    ActiveSupport.to_time_preserves_timezone = true
+
+    # Require `belongs_to` associations by default. Previous versions had false.
+    Rails.application.config.active_record.belongs_to_required_by_default = true
+
+    # Do not halt callback chains when a callback returns false. Previous versions had true.
+    ActiveSupport.halt_callback_chains_on_return_false = false
+    # 
+    ############################
+
 end
