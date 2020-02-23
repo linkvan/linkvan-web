1fdef24e8648788596c36b5e35a204df937bb9f3
Fabio Lima <fabionl@gmail.com>
2020-02-23 10:49:40 -0800

    Solving deprecation warnings, changed before_filter to before_action on controllers.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -4,7 +4,6 @@ class Api::ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	# protect_from_forgery with: :exception
 	respond_to :json
-	#   before_filter :allow_iframe_requests
 	before_action :require_signin
 
 #   add_breadcrumb "Facilities", :root_path
