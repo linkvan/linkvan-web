42f9faae9d08d3d973565a03b5f8089b8e3fa596
Fabio Lima <fabionl@gmail.com>
2020-03-01 12:45:07 -0800

    Added a controller test to help debugging issues that were happening when updating to Rails 6.

--- spec/controllers/facilities_controller_spec.rb
+++ spec/controllers/facilities_controller_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe FacilitiesController, type: :controller do
+  fixtures :facilities
+
   describe 'GET index' do
-    it 'works' do
+    let(:open_all_day) { facilities(:open_all_day) } 
+
+    it 'renders the index template' do
       get :index
       expect(response).to have_http_status(:ok)
+      expect(response).to render_template("index")
     end
-    
-    # it "assigns @teams" do
-    #   team = Team.create
-    #   get :index
-    #   expect(response).to have_http_status(:created
-    #   expect(assigns(:teams)).to eq([team])
-    # end
 
-    # it "renders the index template" do
-    #   get :index
-    #   expect(response).to render_template("index")
-    # end
+    it 'assigns @facilities' do
+      # facility = Facility.create
+      get :index
+      expect(assigns(:facilities)).to include(open_all_day)
+    end
   end
 end
