861ac2ef3bf7a6e8202acbc4824f08e64a0293ab
André Souza <andresouza4@gmail.com>
2020-03-21 14:53:05 -0700

    Fix migrate file

--- db/migrate/20170311000347_add_suitability_to_welcomes.rb
+++ db/migrate/20170311000347_add_suitability_to_welcomes.rb
@@ -2,7 +2,7 @@ class AddSuitabilityToWelcomes < ActiveRecord::Migration
   def change
     change_column :facilities, :welcomes, :string
     Facility.find_each do |f|
-      f.welcomes = f.welcomes.concat(" " + f.suitability)
+      f.welcomes = f.welcomes.concat(" #{f.suitability}")
       f.save
     end
   end
