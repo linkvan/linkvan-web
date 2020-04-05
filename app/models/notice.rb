cabc603744a61c1ad1a2101fb669807b632fa420
André Souza <andresouza4@gmail.com>
2020-04-05 13:59:19 -0700

    Add extra categories to Home page

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,6 +1,8 @@
 class Notice < ActiveRecord::Base
   enum notice_type: {
     general: 'general',
-    covid19: 'covid19'
+    covid19: 'covid19',
+    warming_center: 'warming_center',
+    cooling_center: 'cooling_center'
   }
 end
