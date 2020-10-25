2fb1fd82a15bce2c05efed9a817b296ba508da43
Andre Souza <andresouza4@gmail.com>
2020-10-25 11:52:07 -0700

    Link tutotial videos & Fix notice date render

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,7 @@
 
 //= link_tree ../images
 //= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .css  
+
+//= link How-to-manage-Notices.webm
+//= link How-to-manage-Alerts.webm
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
     def lastUpdate
         @notice = Notice.covid19.where(published: true).last.updated_at.strftime("%b %-d, %Y")
-        render text: @notice
       end
 end
