7ad634bc32b28f86acf3f6687228360877fab0ba
Andre Souza <andresouza4@gmail.com>
2021-01-16 11:30:01 -0800

    Convert date to PST

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
+  TIMEZONE = "Pacific Time (US & Canada)".freeze
+
   def lastUpdate
-    [
-      Facility.order(updated_at: :desc).first.updated_at,
-      Notice.order(updated_at: :desc).first&.updated_at
-    ].reject(&:nil?).max.strftime("%b %-d, %Y")
+    displayDate(
+      [
+        Facility.order(updated_at: :desc).first.updated_at,
+        Notice.order(updated_at: :desc).first&.updated_at
+      ].reject(&:nil?).max
+    ).strftime("%b %-d, %Y")
+  end
+
+  def displayDate(date)
+    date.in_time_zone(TIMEZONE)
   end
 end
